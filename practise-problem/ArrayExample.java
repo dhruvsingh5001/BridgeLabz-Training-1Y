2002640d9e1eb2c486613b3afde5f88aa1425dfe
dhruvsingh5001 <dhruvvsingh7819@gmail.com>
2026-04-22 14:14:53 +0530

    22-04-2026

--- practise-problem/ArrayExample.java
+++
@@ -1,6 +0,0 @@
-class ArrayExample{
-public static void main(String args[]){
-int arr[] ={1,2,3,4,5,6,7,8,9,12};
-System.out.println(arr[3]);
-}
-}
--- practise-problem/ForLoopDemo.java
+++
@@ -1,9 +0,0 @@
-class ForLoopDemo{
-public static void main(String[] args){ //--main method
-
-
-for (int i = 0; i <= 5; i++) {
-    System.out.println(i);
-}
-}
-}
